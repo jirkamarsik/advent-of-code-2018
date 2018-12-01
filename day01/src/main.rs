e95d118428b0383accaab5fc017e7ebc5d23bdda
Jirka Marsik <jiri.marsik89@gmail.com>
2018-12-01 21:16:47 +0100

    Day 1, part 2

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -12,17 +12,45 @@ fn parse_input() -> Vec<i32> {
     freq_changes
 }
 
-fn solve(freq_changes: Vec<i32>) -> i32 {
-    let mut frequency = 0;
+fn final_frequency(freq_changes: &Vec<i32>) -> i32 {
+    freq_changes.iter().sum()
+}
+
+fn repeating_frequency(freq_changes: &Vec<i32>) -> Option<i32> {
+    let n = freq_changes.len();
+    let mut big_delta = 0;
+    let mut partial_deltas = Vec::with_capacity(n);
 
     for delta in freq_changes {
-        frequency += delta;
+        partial_deltas.push(big_delta);
+        big_delta += delta;
     }
 
-    frequency
+    let mut best_candidate = None;
+
+    for i in 0..n {
+        for j in 0..n {
+            if i != j {
+                let delta = partial_deltas[j] - partial_deltas[i];
+                if (delta % big_delta == 0) && (delta / big_delta >= 0) {
+                    let new_candidate = (delta / big_delta, i);
+                    best_candidate = match best_candidate {
+                        None => Some(new_candidate),
+                        Some(best_candidate) => Some(best_candidate.min(new_candidate)),
+                    };
+                }
+            }
+        }
+    }
+
+    best_candidate.map(|(iterations, index)| iterations * big_delta + partial_deltas[index])
 }
 
 fn main() {
     let input = parse_input();
-    println!("{}", solve(input));
+    println!("The final frequency is {}.", final_frequency(&input));
+    match repeating_frequency(&input) {
+        Some(frequency) => println!("The first repeating frequency is {}.", frequency),
+        None => println!("The frequencies will never repeat!"),
+    }
 }
