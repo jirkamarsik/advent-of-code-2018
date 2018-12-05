cb2af1f0aa49418f405d3158b4941f894df23c3d
Jirka Marsik <jiri.marsik89@gmail.com>
2018-12-05 20:34:36 +0100

    Improved Day 5 solution to use a stack

--- day05/src/main.rs
+++ day05/src/main.rs
@@ -1,116 +1,55 @@
 use std::collections::HashMap;
 use std::io::Read;
 
-struct Cursors {
-    left: usize,
-    right: usize,
-}
-
-fn parse_input() -> Vec<u8> {
+fn parse_input() -> String {
     let mut buffer = String::new();
     std::io::stdin().read_to_string(&mut buffer).unwrap();
-    buffer.trim().as_bytes().to_vec()
+    buffer.trim().to_owned()
 }
 
-fn is_opposite_unit(a: u8, b: u8) -> bool {
-    let a = a as char;
-    let b = b as char;
-    (a.to_ascii_uppercase() == b && b.to_ascii_lowercase() == a)
-        || (a.to_ascii_lowercase() == b && b.to_ascii_uppercase() == a)
+fn is_opposite_unit(a: &char, b: &char) -> bool {
+    (&a.to_ascii_uppercase() == b && &b.to_ascii_lowercase() == a)
+        || (&a.to_ascii_lowercase() == b && &b.to_ascii_uppercase() == a)
 }
 
-fn move_left(alive_map: &[bool], pos: usize) -> Option<usize> {
-    for new_pos in (0..pos).rev() {
-        if alive_map[new_pos] {
-            return Some(new_pos)
+fn reduce_polymer<I>(polymer: I, capacity: usize) -> usize
+where
+    I: Iterator<Item = char>,
+{
+    let mut unit_stack: Vec<char> = Vec::with_capacity(capacity);
+
+    for unit in polymer {
+        match unit_stack.pop() {
+            Some(opposite_unit) if is_opposite_unit(&unit, &opposite_unit) => (),
+            Some(other_unit) => {
+                unit_stack.push(other_unit);
+                unit_stack.push(unit);
+            }
+            None => unit_stack.push(unit),
         }
     }
-    None
+
+    unit_stack.len()
 }
 
-fn move_right(alive_map: &[bool], pos: usize) -> Option<usize> {
-    for new_pos in pos + 1..alive_map.len() {
-        if alive_map[new_pos] {
-            return Some(new_pos)
-        }
-    }
-    None
-}
+// fn kill_unit<I>(polymer: I, lower_unit: char, upper_unit: char) -> impl Iterator<Item = char>
+// where I: Iterator<Item = char>
+// {
+//     polymer.filter(|&unit| unit == lower_unit || unit == upper_unit)
+// }
 
-fn move_both_left(alive_map: &[bool], cursors: &Cursors) -> Option<Cursors> {
-    match move_left(alive_map, cursors.left) {
-        Some(left) => Some(Cursors { left, right: cursors.left }),
-        None       => None,
-    }
-}
-
-fn move_both_right(alive_map: &[bool], cursors: &Cursors) -> Option<Cursors> {
-    match move_right(alive_map, cursors.right) {
-        Some(right) => Some(Cursors { left: cursors.right, right }),
-        None        => None,
-    }
-}
-
-fn move_both_out(alive_map: &[bool], cursors: &Cursors) -> Option<Cursors> {
-    match (move_left(alive_map, cursors.left), move_right(alive_map, cursors.right)) {
-        (Some(left), Some(right)) => Some(Cursors { left, right }),
-        (Some(left), None)        => move_both_left(alive_map, &Cursors { left, right: cursors.left }),
-        (None,       Some(right)) => move_both_right(alive_map, &Cursors { left: cursors.right, right }),
-        (None,       None)        => None,
-    }
-}
-
-fn build_alive_map(polymer: &[u8]) -> Vec<bool> {
-    let mut alive_map = Vec::with_capacity(polymer.len());
-    for _ in 0..polymer.len() {
-        alive_map.push(true);
-    }
-    alive_map
-}
-
-fn reduce_polymer(polymer: &[u8], alive_map: &mut [bool]) {
-    let mut cursors_opt = Some(Cursors { left: 0, right: 1 });
-    while let Some(cursors) = cursors_opt {
-        if is_opposite_unit(polymer[cursors.left], polymer[cursors.right]) {
-            alive_map[cursors.left] = false;
-            alive_map[cursors.right] = false;
-            cursors_opt = move_both_out(alive_map, &cursors);
-        } else {
-            cursors_opt = move_both_right(alive_map, &cursors);
-        }
-    }
-}
-
-fn polymer_length(alive_map: &[bool]) -> usize {
-    alive_map.iter().filter(|&&live| live).count()
-}
-
-fn kill_unit(polymer: &[u8], alive_map: &mut [bool], lower_unit: u8, upper_unit: u8) {
-    for (&unit, alive) in polymer.iter().zip(alive_map.iter_mut()) {
-        if unit == lower_unit || unit == upper_unit {
-            *alive = false;
-        }
-    }
-}
-
-fn reset_polymer(alive_map: &mut [bool]) {
-    for alive in alive_map.iter_mut() {
-        *alive = true;
-    }
-}
-
-fn find_problematic_unit(polymer: &[u8], alive_map: &mut [bool]) -> (String, usize) {
+fn find_problematic_unit(polymer: &String) -> (String, usize) {
     let mut scores = HashMap::new();
     // for upper_unit in 'A'..'Z' {
     for upper_unit in 0x41..0x5B {
-        let lower_unit = (upper_unit as char).to_ascii_lowercase() as u8;
-        reset_polymer(alive_map);
-        kill_unit(polymer, alive_map, lower_unit, upper_unit);
-        reduce_polymer(polymer, alive_map);
-        scores.insert(
-            format!("{}/{}", upper_unit as char, lower_unit as char),
-            polymer_length(alive_map),
-        );
+        // How to trick the compiler in inferring the type 'u8' for 'upper_unit'?
+        let upper_unit = (upper_unit as u8) as char;
+        let lower_unit = upper_unit.to_ascii_lowercase();
+        let clean_polymer = polymer
+            .chars()
+            .filter(|&unit| unit != lower_unit && unit != upper_unit);
+        let final_length = reduce_polymer(clean_polymer, polymer.len());
+        scores.insert(format!("{}/{}", upper_unit, lower_unit), final_length);
     }
     let (unit, &length) = scores.iter().min_by_key(|&(_, &score)| score).unwrap();
     (unit.clone(), length)
@@ -118,13 +57,11 @@ fn find_problematic_unit(polymer: &[u8], alive_map: &mut [bool]) -> (String, usi
 
 fn main() {
     let polymer = parse_input();
-    let mut alive_map = build_alive_map(&polymer);
-    reduce_polymer(&polymer, &mut alive_map);
     println!(
         "The resulting polymer is {} units long!",
-        polymer_length(&alive_map)
+        reduce_polymer(polymer.chars(), polymer.len())
     );
-    let (problematic_unit, length) = find_problematic_unit(&polymer, &mut alive_map);
+    let (problematic_unit, length) = find_problematic_unit(&polymer);
     println!(
         "After removing the problematic unit {}, the resulting polymer is {} units long!",
         problematic_unit, length
